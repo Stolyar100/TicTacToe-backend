8127fff436bb8663c189ad1e88de1c24025ff40b
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-06-22 23:34:19 +0300

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const UserRouter = require('./routers/user-router');
+
+dotenv.config();
+const { PORT } = process.env;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/auth', UserRouter);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log('Server started on PORT =', PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
