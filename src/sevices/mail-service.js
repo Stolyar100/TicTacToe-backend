514e4e4db78975209a221457c8f01a204616b5d6
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-06-28 20:27:04 +0300

    Implement sendActivationMail MailService function

--- src/sevices/mail-service.js
+++ src/sevices/mail-service.js
@@ -1,3 +1,32 @@
-class MailService {}
+const nodemailer = require('nodemailer');
+
+class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: true,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to,
+      subject: `TicTacToe account activation ${process.env.API_URL}`,
+      text: '',
+      html: `
+      <div>
+        <h1>For Activation folow the link</h1>
+        <a href="${link}">${link}</a>
+      </div>
+      `,
+    });
+  }
+}
 
 module.exports = new MailService();
