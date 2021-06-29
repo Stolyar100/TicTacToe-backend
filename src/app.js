f5f1a26c7ae5bad7a8550963a39e4787be42372d
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-06-29 19:53:01 +0300

    implemented error middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const UserRouter = require('./routers/user-router');
+const errorMiddleware = require('./middlewares/error-middleware');
 
 const { PORT } = process.env;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 app.use('/auth', UserRouter);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
