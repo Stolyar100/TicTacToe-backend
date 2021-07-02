5d0575acd3507cdaabaa2d1e02babd0de0f80f6e
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-07-02 23:05:53 +0300

    apply middleware to  test endpoint

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { body } = require('express-validator');
 const UserController = require('../controllers/user-controller');
+const authMiddleware = require('../middlewares/auth-middleware');
 
 const UserRouter = new Router();
 
@@ -14,6 +15,6 @@ UserRouter.post('/login', UserController.login);
 UserRouter.post('/logout', UserController.logout);
 UserRouter.get('/activate/:link', UserController.activate);
 UserRouter.get('/refresh', UserController.refresh);
-UserRouter.get('/users', UserController.getUsers);
+UserRouter.get('/users', authMiddleware, UserController.getUsers);
 
 module.exports = UserRouter;
