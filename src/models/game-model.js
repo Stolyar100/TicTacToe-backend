493ff5cc94a9ae29149b98fa7f9b0a3e2720b5ec
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-07-29 18:32:39 +0300

    Update game-model

--- src/models/game-model.js
+++ src/models/game-model.js
@@ -3,28 +3,26 @@ const { Schema, model } = require('mongoose');
 const gameSymbols = ['x', 'o'];
 const fieldState = [...gameSymbols, ''];
 
-const fieldSchema = new Schema({
-  state: { type: String, enum: fieldState, default: '' },
-});
-
 const PlayerSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   symbol: { type: String, enum: gameSymbols, required: true },
 });
 
+const field = { type: String, enum: fieldState, default: '' };
+
 const GameSchema = new Schema({
   inviteCode: { type: String, unique: true, required: true },
   players: [PlayerSchema],
   gameField: {
-    0: { type: fieldSchema, default: {} },
-    1: { type: fieldSchema, default: {} },
-    2: { type: fieldSchema, default: {} },
-    3: { type: fieldSchema, default: {} },
-    4: { type: fieldSchema, default: {} },
-    5: { type: fieldSchema, default: {} },
-    6: { type: fieldSchema, default: {} },
-    7: { type: fieldSchema, default: {} },
-    8: { type: fieldSchema, default: {} },
+    0: field,
+    1: field,
+    2: field,
+    3: field,
+    4: field,
+    5: field,
+    6: field,
+    7: field,
+    8: field,
   },
   currentMove: { type: Schema.Types.ObjectId, ref: 'User' },
 });
