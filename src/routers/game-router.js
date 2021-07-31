ac3e2431ade90b20ad18ceef20d1dc62d2edfdad
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-01 00:34:53 +0300

    Add express validation of create game request

--- src/routers/game-router.js
+++ src/routers/game-router.js
@@ -1,10 +1,28 @@
 const { Router } = require('express');
+const { body } = require('express-validator');
 const GameController = require('../controllers/game-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
+const GameSymbols = require('../shared/game-symbols');
 
 const GameRouter = new Router();
 
-GameRouter.post('/', authMiddleware, GameController.createGame);
+GameRouter.post(
+  '/',
+  [
+    body('symbol')
+      .exists()
+      .withMessage('symbol field is required')
+      .bail()
+      .notEmpty()
+      .withMessage('symbol field can not be empty')
+      .isIn(GameSymbols.symbolEnum)
+      .withMessage(
+        `symbol field value should be one of ${GameSymbols.symbolEnum}`
+      ),
+  ],
+  authMiddleware,
+  GameController.createGame
+);
 GameRouter.post('/:inviteCode', authMiddleware, GameController.joinGame);
 GameRouter.get('/:inviteCode', authMiddleware, GameController.getGame);
 GameRouter.put('/:inviteCode', authMiddleware, GameController.makeMove);
