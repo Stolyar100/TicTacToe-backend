3797d04269f1bfdb855c69e131435469180fa67e
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-02 21:34:42 +0300

    Separate checking validation results logic from game-controller to new function, and controller function binding

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -3,12 +3,14 @@ const ApiError = require('../exceptions/api-error');
 const GameService = require('../services/game-service');
 
 class GameController {
+  constructor() {
+    this.createGame = this.createGame.bind(this);
+    this.joinGame = this.joinGame.bind(this);
+  }
+
   async createGame(req, res, next) {
     try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(ApiError.BadRequest('Validation error', errors.array()));
-      }
+      this._handleValidationResults(req, next);
       const { symbol } = req.body;
       const userId = req.user.id;
 
@@ -17,6 +19,13 @@ class GameController {
       return res.status(201).json(gameData);
     } catch (e) {
       next(e);
+    }
+  }
+
+  _handleValidationResults(req, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return next(ApiError.BadRequest('Validation error', errors.array()));
     }
   }
 }
