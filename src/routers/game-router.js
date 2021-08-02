36dd44deed08731442b716a9c67b49cb13759571
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-02 21:41:30 +0300

    Implement schema validation in game-router

--- src/routers/game-router.js
+++ src/routers/game-router.js
@@ -1,29 +1,23 @@
 const { Router } = require('express');
-const { body } = require('express-validator');
+const { checkSchema } = require('express-validator');
 const GameController = require('../controllers/game-controller');
 const authMiddleware = require('../middlewares/auth-middleware');
-const GameSymbols = require('../shared/game-symbols');
+const gameValidationSchema = require('../validation-schemas/game-validation-schema');
 
 const GameRouter = new Router();
 
 GameRouter.post(
   '/',
-  [
-    body('symbol')
-      .exists()
-      .withMessage('symbol field is required')
-      .bail()
-      .notEmpty()
-      .withMessage('symbol field can not be empty')
-      .isIn(GameSymbols.symbolEnum)
-      .withMessage(
-        `symbol field value should be one of ${GameSymbols.symbolEnum}`
-      ),
-  ],
+  checkSchema(gameValidationSchema),
   authMiddleware,
   GameController.createGame
 );
-GameRouter.post('/:inviteCode', authMiddleware, GameController.joinGame);
+GameRouter.post(
+  '/:inviteCode',
+  checkSchema(gameValidationSchema),
+  authMiddleware,
+  GameController.joinGame
+);
 GameRouter.get('/:inviteCode', authMiddleware, GameController.getGame);
 GameRouter.put('/:inviteCode', authMiddleware, GameController.makeMove);
 GameRouter.delete('/:inviteCode', authMiddleware, GameController.deleteGame);
