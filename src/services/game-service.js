c8ea21db3ebd8428027cc6048cc5f84e4fbd35b8
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-03 22:51:56 +0300

    Add joinGame method in game-service, fix gameDto constructor calls

--- src/services/game-service.js
+++ src/services/game-service.js
@@ -1,6 +1,8 @@
 const GameDto = require('../dtos/game-dto');
 const ApiError = require('../exceptions/api-error');
 const GameModel = require('../models/game-model');
+const UserModel = require('../models/user-model');
+const GameSymbols = require('../shared/game-symbols');
 
 class GameService {
   async createGame(userId, symbol) {
@@ -9,14 +11,40 @@ class GameService {
       inviteCode,
       players: [{ user: userId, symbol }],
     });
-    const gameDto = new GameDto(game, userId);
+    const gameDto = new GameDto({ game, requestsUserId: userId });
 
     return { ...gameDto };
+  }
+
+  async joinGame(inviteCode, userId) {
+    const gameToJoin = await GameModel.findOne({ inviteCode });
+    if (!gameToJoin) {
+      throw ApiError.BadRequest('invalid invite code');
+    }
+    const { user: opponentId, symbol: opponentSymbol } = gameToJoin.players[0];
+    const opponentData = await UserModel.findById(opponentId);
+    const currentUserSymbol = GameSymbols.getOppositeSymbol(opponentSymbol);
+
+    gameToJoin.players.push({ user: userId, symbol: currentUserSymbol });
+    gameToJoin.currentMove = this._chooseRandomElement(gameToJoin.players);
+    await gameToJoin.save();
+
+    const gameDto = new GameDto({
+      game: gameToJoin,
+      requestsUserId: userId,
+      opponentData,
+    });
+    return gameDto;
   }
 
   _generateInviteCode() {
     const code = Math.random().toString(16).substr(2, 7);
     return code;
+  }
+
+  _chooseRandomElement(array) {
+    const randomId = Math.floor(Math.random() * array.length);
+    return array[randomId];
   }
 }
 
