a8b400fe2c38000932897c4da973f6869b59594d
Oleh Marchuk <olehmarchukdev@gmail.com>
2021-08-03 22:43:35 +0300

    Remove symbol param from joinGame controller

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -24,12 +24,10 @@ class GameController {
 
   async joinGame(res, req, next) {
     try {
-      this._handleValidationResults(req, next);
       const { inviteCode } = req.params;
       const userId = req.user.id;
-      const { symbol } = req.body;
 
-      const gameData = await GameService.joinGame(inviteCode, userId, symbol);
+      const gameData = await GameService.joinGame(inviteCode, userId);
       return res.status(200).json(gameData);
     } catch (e) {
       next(e);
